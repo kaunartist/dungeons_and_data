120df69137b21d156f0b4e18c5fee4eeeab4e670
Jonathan Rudenberg <jonathan@titanous.com>
2011-01-29 11:05:10 -0500

    Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DungeonsAndData::Application.config.secret_token = '0756044a35dda0f9d217758c9e8481201fad1d59c3c5b3c1e86595d000a2fbaf955b205e001db03b9d63863b61a2e6877f7ccdf7bfcd514dfd343fb0976b96aa'
