91f4f297bb5b46fec2502108d0b36a7848cc208b
Jonathan Rudenberg <jonathan@titanous.com>
2011-01-29 13:15:23 -0500

    Datapoint geocoding and validation

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.3'
 
 gem 'pg'
 gem 'inherited_resources', '~> 1.2.1'
+gem 'rails-geocoder', :require => 'geocoder'
 
 group :development, :test do
   gem 'silent-postgres'
--- app/models/datapoint.rb
+++ app/models/datapoint.rb
@@ -3,4 +3,23 @@ class Datapoint < ActiveRecord::Base
   belongs_to :quest
   belongs_to :user
 
+  attr_protected :quest_id, :user_id, :location_source
+
+  validates_presence_of :latitude, :longitude, :location_source, :quest_id,
+                        :user_id
+
+  attr_accessor :address
+  geocoded_by :address
+  before_validation :set_coordinates
+
+  def set_coordinates
+    if self.address
+      fetch_coordinates
+      self.location_source = 'google'
+    elsif self.longitude && self.latitude && self.location_source.empty?
+      self.location_source = 'geolocation'
+    end
+  end
+
+
 end
