1ed4357f38c2a723ff96fe529fc290f5819d7a4b
Jonathan Rudenberg <jonathan@titanous.com>
2011-01-29 12:32:31 -0500

    Quest validations

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -4,4 +4,7 @@ class Quest < ActiveRecord::Base
   has_many :comments
   has_many :datapoints
 
+  validates_presence_of :title, :description, :minimum_datapoints, :expired_at
+  # TODO: minimum expired_at validation
+
 end
